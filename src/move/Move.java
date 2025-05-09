424af15bf5234e7162f5122657528071ef788682
Md Mahfuz <mdmahfuzurr232@gmail.com.>
2025-05-09 15:15:38 +0600

    initial commit

---
+++ src/move/Move.java
@@ -0,0 +1,26 @@
+
+
+package move;
+
+import java.util.Scanner;
+
+public class Move {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Calculating the sum
+        double sum = num1 + num2;
+
+        // Displaying the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
+    }
+}
